ab942f20d8fcd6947ad0021f277d7593b58d0bee
yuryjin <48969408+yuryjin@users.noreply.github.com>
2021-01-28 12:13:54 +0300

    changedsettings

--- data/settings.py
+++ data/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*3o8*$t5^lmpxrbx_y!eusygbku&uc4ue6659lo-)#-f!2+o4d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['timeapp1.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,4 +126,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
